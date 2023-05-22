90b023207e530b9d3b5c2453a6880f83d885c3f5
Natan Alonso <77931510+Natan-Alonso@users.noreply.github.com>
2023-05-22 16:44:35 -0300

    aula 3

---
+++ src/components/EmptyMemories.tsx
@@ -0,0 +1,19 @@
+export function EmptyMemories() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a{' '}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          className="underline hover:text-gray-50"
+          href="https://rocketseat.com.br"
+        >
+          {' '}
+          criar agora
+        </a>
+        !
+      </p>
+    </div>
+  )
+}
