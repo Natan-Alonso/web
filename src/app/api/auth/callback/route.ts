39e0942e2b09e893a1c90f78640e60f644d732de
Natan Alonso <77931510+Natan-Alonso@users.noreply.github.com>
2023-05-25 16:46:33 -0300

    aula 3/4

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -5,13 +5,17 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const code = searchParams.get('code')
 
+  const redirectTo = request.cookies.get('redirectTo')?.value
+
   const registerResponse = await api.post('/register', {
     code,
   })
 
   const { token } = registerResponse.data
 
-  const redirectURL = new URL('/', request.url)
+  console.log(token)
+
+  const redirectURL = redirectTo ?? new URL('/', request.url)
 
   const cookieExpireInSeconds = 60 * 60 * 24 * 30
 
